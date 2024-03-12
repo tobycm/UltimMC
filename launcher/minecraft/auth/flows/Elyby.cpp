51fff1f02c89afd389abd77c711a4cda0c863129
Neptune <neptune650@proton.me>
2024-03-11 20:58:42 -0500

    UltimMC: Use proper Ely.by profile

--- launcher/minecraft/auth/flows/Elyby.cpp
+++ launcher/minecraft/auth/flows/Elyby.cpp
@@ -1,14 +1,14 @@
 #include "Elyby.h"
 
 #include "minecraft/auth/steps/YggdrasilStep.h"
-#include "minecraft/auth/steps/GetSkinStep.h"
+#include "minecraft/auth/steps/ElybyProfileStep.h"
 
 ElybyRefresh::ElybyRefresh(
     AccountData *data,
     QObject *parent
 ) : AuthFlow(data, parent) {
     m_steps.append(new YggdrasilStep(m_data, QString()));
-    m_steps.append(new GetSkinStep(m_data));
+    m_steps.append(new ElybyProfileStep(m_data));
 }
 
 ElybyLogin::ElybyLogin(
@@ -17,5 +17,5 @@ ElybyLogin::ElybyLogin(
     QObject *parent
 ): AuthFlow(data, parent), m_password(password) {
     m_steps.append(new YggdrasilStep(m_data, m_password));
-    m_steps.append(new GetSkinStep(m_data));
+    m_steps.append(new ElybyProfileStep(m_data));
 }
---
+++ launcher/minecraft/auth/steps/ElybyProfileStep.cpp
@@ -0,0 +1,90 @@
+#include "ElybyProfileStep.h"
+
+#include <QNetworkRequest>
+
+#include "minecraft/auth/AuthRequest.h"
+#include "minecraft/auth/Parsers.h"
+
+ElybyProfileStep::ElybyProfileStep(AccountData* data) : AuthStep(data) {
+
+}
+
+ElybyProfileStep::~ElybyProfileStep() noexcept = default;
+
+QString ElybyProfileStep::describe() {
+    return tr("Fetching the Ely.by profile.");
+}
+
+
+void ElybyProfileStep::perform() {
+    auto url = QUrl(QString("https://authserver.ely.by/api/users/profiles/minecraft/%1").arg(m_data->userName()).toUtf8());
+    QNetworkRequest request = QNetworkRequest(url);
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+
+    AuthRequest *requestor = new AuthRequest(this);
+    connect(requestor, &AuthRequest::finished, this, &ElybyProfileStep::onRequestDone);
+    requestor->get(request);
+}
+
+void ElybyProfileStep::rehydrate() {
+    // NOOP, for now. We only save bools and there's nothing to check.
+}
+
+void ElybyProfileStep::onRequestDone(
+    QNetworkReply::NetworkError error,
+    QByteArray data,
+    QList<QNetworkReply::RawHeaderPair> headers
+) {
+    auto requestor = qobject_cast<AuthRequest *>(QObject::sender());
+    requestor->deleteLater();
+
+#ifndef NDEBUG
+    qDebug() << data;
+#endif
+    if (error == QNetworkReply::ContentNotFoundError) {
+        // NOTE: Succeed even if we do not have a profile. This is a valid account state.
+        if(m_data->type == AccountType::Mojang) {
+            m_data->minecraftEntitlement.canPlayMinecraft = false;
+            m_data->minecraftEntitlement.ownsMinecraft = false;
+        }
+        m_data->minecraftProfile = MinecraftProfile();
+        emit finished(
+            AccountTaskState::STATE_SUCCEEDED,
+            tr("Account has no Minecraft profile.")
+        );
+        return;
+    }
+    if (error != QNetworkReply::NoError) {
+        qWarning() << "Error getting profile:";
+        qWarning() << " HTTP Status:        " << requestor->httpStatus_;
+        qWarning() << " Internal error no.: " << error;
+        qWarning() << " Error string:       " << requestor->errorString_;
+
+        qWarning() << " Response:";
+        qWarning() << QString::fromUtf8(data);
+
+        emit finished(
+            AccountTaskState::STATE_FAILED_SOFT,
+            tr("Minecraft Java profile acquisition failed.")
+        );
+        return;
+    }
+    if(!Parsers::parseMinecraftProfile(data, m_data->minecraftProfile)) {
+        m_data->minecraftProfile = MinecraftProfile();
+        emit finished(
+            AccountTaskState::STATE_FAILED_SOFT,
+            tr("Minecraft Java profile response could not be parsed")
+        );
+        return;
+    }
+
+    if(m_data->type == AccountType::Mojang) {
+        auto validProfile = m_data->minecraftProfile.validity == Katabasis::Validity::Certain;
+        m_data->minecraftEntitlement.canPlayMinecraft = validProfile;
+        m_data->minecraftEntitlement.ownsMinecraft = validProfile;
+    }
+    emit finished(
+        AccountTaskState::STATE_WORKING,
+        tr("Minecraft Java profile acquisition succeeded.")
+    );
+}
