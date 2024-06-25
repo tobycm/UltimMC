e537265adee0c3914598bc953c339514dc5744b4
Neptune <neptune650@proton.me>
2024-06-24 23:03:53 -0500

    UltimMC: Update MSA client ID.

--- secrets/Secrets.cpp
+++ secrets/Secrets.cpp
@@ -28,7 +28,7 @@ namespace {
  * If you intend to base your own launcher on this code, take care and customize this to obfuscate the client ID, so it cannot be trivially found by casual attackers.
  */
 
-QString MSAClientID = "f4404707-7bbe-4e40-80ba-85fb2bb825a1";
+QString MSAClientID = "d77ef04a-390a-40c6-a32d-700a4549ffa6";
 }
 
 namespace Secrets {
